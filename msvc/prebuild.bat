ca4feb0f35154fa7747f2da523af2c8994ba4375
Antoine <end2endzone@gmail.com>
2017-11-25 17:32:42 -0500

    Modified bin2cpp_unittest project to use testfilegenerator for generating test files for unit tests. Created the following new tests: testHtml100000, testPredictable100000, testRandom100000, testSequential10MB, testText100000

--- msvc/prebuild.bat
+++ msvc/prebuild.bat
@@ -1,15 +1,50 @@
 @echo off
 :: %1 is $(ConfigurationName)
-:: %2 is $(ConfigurationName)
+:: %2 is $(Platform)
 
 set BIN2CPP_EXE=.\%2\%1\bin2cpp.exe
+set TESTFILEGENERATOR_EXE=.\%2\%1\testfilegenerator.exe
 
-::testBinaryFile prebuild
-set OUTDIR=.\generated_files\testBinaryFile
+set TEST_NAME=testRandom100000
+set OUTDIR=.\generated_files\%TEST_NAME%
 mkdir %OUTDIR% 1>NUL 2>NUL
-%BIN2CPP_EXE% ..\docs\icon.xcf %OUTDIR% EmbeddedBinaryFile.h icon -override 250
+%TESTFILEGENERATOR_EXE% --file=%OUTDIR%\%TEST_NAME%.bin --size=100000 --fill=random
+%BIN2CPP_EXE% %OUTDIR%\%TEST_NAME%.bin %OUTDIR% _%TEST_NAME%.h %TEST_NAME% 450 -override
 
-::testTextFile prebuild
-set OUTDIR=.\generated_files\testTextFile
+set TEST_NAME=testPredictable100000
+set OUTDIR=.\generated_files\%TEST_NAME%
 mkdir %OUTDIR% 1>NUL 2>NUL
-%BIN2CPP_EXE% ..\README.md %OUTDIR% EmbeddedTextFile.h readme -override 250
+%TESTFILEGENERATOR_EXE% --file=%OUTDIR%\%TEST_NAME%.bin --size=100000 --fill=predictable
+%BIN2CPP_EXE% %OUTDIR%\%TEST_NAME%.bin %OUTDIR% _%TEST_NAME%.h %TEST_NAME% 450 -override
+
+set TEST_NAME=testHtml100000
+set OUTDIR=.\generated_files\%TEST_NAME%
+mkdir %OUTDIR% 1>NUL 2>NUL
+%TESTFILEGENERATOR_EXE% --file=%OUTDIR%\%TEST_NAME%.bin --size=100000 --fill=html
+%BIN2CPP_EXE% %OUTDIR%\%TEST_NAME%.bin %OUTDIR% _%TEST_NAME%.h %TEST_NAME% 450 -override
+
+set TEST_NAME=testText100000
+set OUTDIR=.\generated_files\%TEST_NAME%
+mkdir %OUTDIR% 1>NUL 2>NUL
+%TESTFILEGENERATOR_EXE% --file=%OUTDIR%\%TEST_NAME%.bin --size=100000 --fill=text
+%BIN2CPP_EXE% %OUTDIR%\%TEST_NAME%.bin %OUTDIR% _%TEST_NAME%.h %TEST_NAME% 450 -override
+
+set TEST_NAME=testSequential10MB
+set OUTDIR=.\generated_files\%TEST_NAME%
+mkdir %OUTDIR% 1>NUL 2>NUL
+%TESTFILEGENERATOR_EXE% --file=%OUTDIR%\%TEST_NAME%.bin --size=10485760 --fill=sequential
+%BIN2CPP_EXE% %OUTDIR%\%TEST_NAME%.bin %OUTDIR% _%TEST_NAME%.h %TEST_NAME% 450 -override
+
+REM   set TEST_NAME=testSequential20MB
+REM   set OUTDIR=.\generated_files\%TEST_NAME%
+REM   mkdir %OUTDIR% 1>NUL 2>NUL
+REM   %TESTFILEGENERATOR_EXE% --file=%OUTDIR%\%TEST_NAME%.bin --size=20971520 --fill=sequential
+REM   %BIN2CPP_EXE% %OUTDIR%\%TEST_NAME%.bin %OUTDIR% _%TEST_NAME%.h %TEST_NAME% 450 -override
+REM   
+REM   set TEST_NAME=testSequential30MB
+REM   set OUTDIR=.\generated_files\%TEST_NAME%
+REM   mkdir %OUTDIR% 1>NUL 2>NUL
+REM   %TESTFILEGENERATOR_EXE% --file=%OUTDIR%\%TEST_NAME%.bin --size=31457280 --fill=sequential
+REM   %BIN2CPP_EXE% %OUTDIR%\%TEST_NAME%.bin %OUTDIR% _%TEST_NAME%.h %TEST_NAME% 450 -override
+
+dir >NUL
--- test/TestExtraction.cpp
+++ test/TestExtraction.cpp
@@ -1,8 +1,11 @@
 #include "TestExtraction.h"
 #include "gtesthelper.h"
 
-#include "generated_files\testBinaryFile\EmbeddedBinaryFile.h"
-#include "generated_files\testTextFile\EmbeddedTextFile.h"
+#include "generated_files\testHtml100000\_testHtml100000.h"
+#include "generated_files\testPredictable100000\_testPredictable100000.h"
+#include "generated_files\testRandom100000\_testRandom100000.h"
+#include "generated_files\testSequential10MB\_testSequential10MB.h"
+#include "generated_files\testText100000\_testText100000.h"
 
 gTestHelper & hlp = gTestHelper::getInstance();
 
@@ -14,12 +17,12 @@ void TestExtraction::TearDown()
 {
 }
 
-TEST_F(TestExtraction, testBinaryFile)
+TEST_F(TestExtraction, testHtml100000)
 {
-  static const char * expectedFilePath = "..\\docs\\icon.xcf";
-  static const char * outputFilePath = "generated_files\\testBinaryFile\\icon.output.xcf";
+  static const char * expectedFilePath =  "generated_files\\testHtml100000\\testHtml100000.bin";
+  static const char * outputFilePath =    "generated_files\\testHtml100000\\testHtml100000.output";
 
-  bin2cpp::File & file = bin2cpp::getIconFile();
+  bin2cpp::File & file = bin2cpp::getTestHtml100000File();
   bool extractSuccess = file.save(outputFilePath);
   ASSERT_TRUE(extractSuccess);
 
@@ -29,12 +32,57 @@ TEST_F(TestExtraction, testBinaryFile)
   ASSERT_TRUE(equal) << reason.c_str();
 }
 
-TEST_F(TestExtraction, testTextFile)
+TEST_F(TestExtraction, testPredictable100000)
 {
-  static const char * expectedFilePath = "..\\README.md";
-  static const char * outputFilePath = "generated_files\\testTextFile\\readme.output.md";
+  static const char * expectedFilePath =  "generated_files\\testPredictable100000\\testPredictable100000.bin";
+  static const char * outputFilePath =    "generated_files\\testPredictable100000\\testPredictable100000.output";
 
-  bin2cpp::File & file = bin2cpp::getReadmeFile();
+  bin2cpp::File & file = bin2cpp::getTestPredictable100000File();
+  bool extractSuccess = file.save(outputFilePath);
+  ASSERT_TRUE(extractSuccess);
+
+  //assert content is the same
+  std::string reason;
+  bool equal = hlp.isFileEquals(expectedFilePath, outputFilePath, reason);
+  ASSERT_TRUE(equal) << reason.c_str();
+}
+
+TEST_F(TestExtraction, testRandom100000)
+{
+  static const char * expectedFilePath =  "generated_files\\testRandom100000\\testRandom100000.bin";
+  static const char * outputFilePath =    "generated_files\\testRandom100000\\testRandom100000.output";
+
+  bin2cpp::File & file = bin2cpp::getTestRandom100000File();
+  bool extractSuccess = file.save(outputFilePath);
+  ASSERT_TRUE(extractSuccess);
+
+  //assert content is the same
+  std::string reason;
+  bool equal = hlp.isFileEquals(expectedFilePath, outputFilePath, reason);
+  ASSERT_TRUE(equal) << reason.c_str();
+}
+
+TEST_F(TestExtraction, testSequential10MB)
+{
+  static const char * expectedFilePath =  "generated_files\\testSequential10MB\\testSequential10MB.bin";
+  static const char * outputFilePath =    "generated_files\\testSequential10MB\\testSequential10MB.output";
+
+  bin2cpp::File & file = bin2cpp::getTestSequential10MBFile();
+  bool extractSuccess = file.save(outputFilePath);
+  ASSERT_TRUE(extractSuccess);
+
+  //assert content is the same
+  std::string reason;
+  bool equal = hlp.isFileEquals(expectedFilePath, outputFilePath, reason);
+  ASSERT_TRUE(equal) << reason.c_str();
+}
+
+TEST_F(TestExtraction, testText100000)
+{
+  static const char * expectedFilePath =  "generated_files\\testText100000\\testText100000.bin";
+  static const char * outputFilePath =    "generated_files\\testText100000\\testText100000.output";
+
+  bin2cpp::File & file = bin2cpp::getTestText100000File();
   bool extractSuccess = file.save(outputFilePath);
   ASSERT_TRUE(extractSuccess);
 
