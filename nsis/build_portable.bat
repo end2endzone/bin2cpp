a83fdd646467530740a187499d4fae61a527b918
Antoine <end2endzone@gmail.com>
2017-11-26 10:59:10 -0500

    Create NSIS installer and portable win32 package (*.zip)

---
+++ nsis/build_portable.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+
+REM **************************************
+REM  Generate ZIP package
+REM **************************************
+
+cd /d %~dp0
+set PRODUCT_PLATFORM=Win32
+
+
+call ..\version_info.bat
+
+set outfile=".\bin\bin2cpp v%PRODUCT_VERSION% %PRODUCT_PLATFORM% portable.zip"
+
+set infiles=
+set infiles=%infiles% "..\msvc\%PRODUCT_PLATFORM%\Release\bin2cpp.exe"
+set infiles=%infiles% "..\msvc\%PRODUCT_PLATFORM%\Release\bin2cpp_unittest.exe"
+set infiles=%infiles% "..\msvc\%PRODUCT_PLATFORM%\Release\testfilegenerator.exe"
+set infiles=%infiles% "..\AUTHORS"
+set infiles=%infiles% "..\CHANGES"
+set infiles=%infiles% "..\LICENSE"
+set infiles=%infiles% "..\README.md"
+
+echo Generating portable install %outfile%
+echo using the following input files:
+echo %infiles%
+echo.
+call "C:\Program Files\7-Zip\7z.exe" a -bd %outfile% %infiles%
+
+pause
---
+++ nsis/build_setup.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM **************************************
+REM  Build Installer (setup) package
+REM **************************************
+
+cd /d %~dp0
+set PRODUCT_PLATFORM=Win32
+
+"C:\Program Files (x86)\NSIS\makensis.exe" /DPLATFORM_INCLUDE=%PRODUCT_PLATFORM%.nsh main.nsi
+
+pause
