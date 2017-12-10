e3b8f5ebf5d63de72d0719b3ca131c1ba937f375
Antoine <end2endzone@gmail.com>
2017-12-10 15:21:59 -0500

    Moved install and test_script to /ci/appveyor/*.bat

---
+++ ci/appveyor/test_script.bat
@@ -0,0 +1,15 @@
+REM ======================================================================
+REM Running unit tests
+REM =======================================================================
+cd /d c:\projects\bin2cpp\msvc
+set path=%PATH%;c:\projects\bin2cpp\msvc\Win32\Release
+bin2cpp_unittest.exe --gtest_filter=-TestExtraction.testRandom100000
+
+REM ======================================================================
+REM Uploading test results to AppVeyor
+REM =======================================================================
+set TEST_RESULT_URL=https://ci.appveyor.com/api/testresults/junit/%APPVEYOR_JOB_ID%
+set TEST_RESULT_FILE=%CD%\bin2cppTest.x86.release.xml
+echo TEST_RESULT_URL=%TEST_RESULT_URL%
+echo TEST_RESULT_FILE=%TEST_RESULT_FILE%
+powershell "(New-Object 'System.Net.WebClient').UploadFile($($env:TEST_RESULT_URL), $($env:TEST_RESULT_FILE))"
