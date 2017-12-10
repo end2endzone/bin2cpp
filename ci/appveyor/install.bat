072f03a04147f4cec0356f7db6c121bc9c91c74f
Antoine <end2endzone@gmail.com>
2017-12-10 15:27:34 -0500

    Replaced REM by echo in appveyor batch files.

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -1,28 +1,28 @@
-REM ======================================================================
-REM Cloning google test repository
-REM =======================================================================
+echo ======================================================================
+echo Cloning google test repository
+echo =======================================================================
 git clone https://github.com/google/googletest.git c:\projects\third_party\googletest
 cd /d c:\projects\third_party\googletest
 git checkout release-1.6.0
 
-REM =======================================================================
-REM Generating google test Visual Studio 2010 solution
-REM =======================================================================
+echo =======================================================================
+echo Generating google test Visual Studio 2010 solution
+echo =======================================================================
 mkdir msvc2010
 cd msvc2010
 cmake -G "Visual Studio 10 2010" -Dgtest_force_shared_crt=ON -DCMAKE_CXX_FLAGS_DEBUG=/MDd -DCMAKE_CXX_FLAGS_RELEASE=/MD "c:\projects\third_party\googletest"
 cd..
 
-REM =======================================================================
-REM Displaying Environnment Variables
-REM =======================================================================
+echo =======================================================================
+echo Displaying Environnment Variables
+echo =======================================================================
 set
 
-REM =======================================================================
-REM Building google test library
-REM =======================================================================
+echo =======================================================================
+echo Building google test library
+echo =======================================================================
 msbuild "msvc2010\gtest.sln" /m /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
-REM =======================================================================
-REM Done
-REM =======================================================================
+echo =======================================================================
+echo Done
+echo =======================================================================
