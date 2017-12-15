f4485fd2ca1a191d1469f96adf36d1fed05d0eed
Antoine <end2endzone@gmail.com>
2017-12-15 12:09:44 -0500

    Moved c:\projects\third_party\googletest git repository to c:\projects\bin2cpp\third_party\googletest for appveyor. Splitted /ci/appveyor/install.bat script into /ci/windowslocal/googletest_install.bat and /ci/appveyor/googletest_build.bat for readability and reusability on a local windows machine.

---
+++ ci/windowslocal/googletest_install.bat
@@ -0,0 +1,54 @@
+@echo off
+
+echo =======================================================================
+echo Finding root folder of repository
+echo =======================================================================
+cd /d %~dp0
+cd ..\..
+set REPOSITORY_ROOT=%cd%
+echo REPOSITORY_ROOT=%REPOSITORY_ROOT%
+echo done.
+echo.
+
+echo ======================================================================
+echo Cloning google test repository
+echo =======================================================================
+git clone https://github.com/google/googletest.git %REPOSITORY_ROOT%\third_party\googletest
+cd /d %REPOSITORY_ROOT%\googletest
+git checkout release-1.6.0
+echo done.
+echo.
+
+echo =======================================================================
+echo Generating google test Visual Studio 2010 solution
+echo =======================================================================
+cd /d %REPOSITORY_ROOT%\third_party\googletest
+
+echo Deleting previous msvc2010 folder (if any)
+rmdir /s/q msvc2010 >NUL 2>NUL
+echo done.
+echo.
+
+echo Creating msvc2010 folder.
+mkdir %REPOSITORY_ROOT%\third_party\googletest\msvc2010
+echo done.
+echo.
+
+echo Launching cmake...
+cd /d %REPOSITORY_ROOT%\third_party\googletest\msvc2010
+cmake -G "Visual Studio 10 2010" -Dgtest_force_shared_crt=ON -DCMAKE_CXX_FLAGS_DEBUG=/MDd -DCMAKE_CXX_FLAGS_RELEASE=/MD "%REPOSITORY_ROOT%\third_party\googletest"
+echo done.
+echo.
+
+echo =======================================================================
+echo Setting environnment variables for googletest
+echo =======================================================================
+set GTEST_DEBUG_LIBRARIES=gtest.lib
+set GTEST_RELEASE_LIBRARIES=gtest.lib
+set GTEST_INCLUDE=%GTEST_HOME%\include
+set GTEST_LIBRARY_DIR=%GTEST_HOME%\msvc2010
+setx GTEST_DEBUG_LIBRARIES %GTEST_DEBUG_LIBRARIES%
+setx GTEST_RELEASE_LIBRARIES %GTEST_RELEASE_LIBRARIES%
+setx GTEST_INCLUDE %GTEST_INCLUDE%
+setx GTEST_LIBRARY_DIR %GTEST_LIBRARY_DIR%
+set GTEST_
