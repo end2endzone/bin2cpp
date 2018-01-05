5203c78f95fc1ce548cfa3ae3a11f620581ad211
Antoine <end2endzone@gmail.com>
2018-01-05 17:11:46 -0500

    Fixed /ci/appveyor/version_info_update.bat which was incorrectly adding spaces after the version value.

--- ci/appveyor/version_info_update.bat
+++ ci/appveyor/version_info_update.bat
@@ -10,11 +10,12 @@ echo REPOSITORY_ROOT=%REPOSITORY_ROOT%
 echo done.
 echo.
 
+set APPVEYOR_BUILD_VERSION=1.2.3
 
 REM **************** version_info.bat ****************
 set OUTPUTFILE=%REPOSITORY_ROOT%\version_info.bat
 echo Updating %OUTPUTFILE%...
-echo set PRODUCT_VERSION=%APPVEYOR_BUILD_VERSION% >%OUTPUTFILE%
+echo set PRODUCT_VERSION=%APPVEYOR_BUILD_VERSION%>%OUTPUTFILE%
 set OUTPUTFILE=
 echo done.
 echo.
@@ -22,10 +23,10 @@ echo.
 REM **************** version_info.h ****************
 set OUTPUTFILE=%REPOSITORY_ROOT%\version_info.h
 echo Updating %OUTPUTFILE%...
-echo #ifndef BINCPP_VERSION_H  >%OUTPUTFILE%
-echo #define BINCPP_VERSION_H >>%OUTPUTFILE%
-echo #define BINCPP_VERSION "%APPVEYOR_BUILD_VERSION%" >>%OUTPUTFILE%
-echo #endif //BINCPP_VERSION_H >>%OUTPUTFILE%
+echo #ifndef BINCPP_VERSION_H>%OUTPUTFILE%
+echo #define BINCPP_VERSION_H>>%OUTPUTFILE%
+echo #define BINCPP_VERSION "%APPVEYOR_BUILD_VERSION%">>%OUTPUTFILE%
+echo #endif //BINCPP_VERSION_H>>%OUTPUTFILE%
 set OUTPUTFILE=
 echo done.
 echo.
@@ -33,7 +34,7 @@ echo.
 REM **************** version_info.nsh ****************
 set OUTPUTFILE=%REPOSITORY_ROOT%\version_info.nsh
 echo Updating %OUTPUTFILE%...
-echo !define PRODUCT_VERSION "%APPVEYOR_BUILD_VERSION%" >%OUTPUTFILE%
+echo !define PRODUCT_VERSION "%APPVEYOR_BUILD_VERSION%">%OUTPUTFILE%
 set OUTPUTFILE=
 echo done.
 echo.
