2a26165eab06875be88e2f72957d8422a01d7abd
Antoine <end2endzone@gmail.com>
2018-01-20 16:08:38 -0500

    Reduced the size of input file for TestEncoding.testEncodingOct() and TestEncoding.testEncodingHex()

--- msvc/generate_test_files.bat
+++ msvc/generate_test_files.bat
@@ -129,14 +129,14 @@ bin2cpp.exe --noheader --file=%OUTDIR%\%TEST_NAME%.bin --output=%OUTDIR% --heade
 set TEST_NAME=testEncodingOct
 set OUTDIR=.\generated_files\%TEST_NAME%
 mkdir %OUTDIR% 1>NUL 2>NUL
-testfilegenerator.exe --file=%OUTDIR%\%TEST_NAME%.bin --size=10000 --fill=sequential
-bin2cpp.exe --noheader --file=%OUTDIR%\%TEST_NAME%.bin --output=%OUTDIR% --headerfile=_%TEST_NAME%.h --identifier=%TEST_NAME%  --chunksize=450 --generator=segment --override --encoding=OcT
+testfilegenerator.exe --file=%OUTDIR%\%TEST_NAME%.bin --size=300 --fill=sequential
+bin2cpp.exe --noheader --file=%OUTDIR%\%TEST_NAME%.bin --output=%OUTDIR% --headerfile=_%TEST_NAME%.h --identifier=%TEST_NAME%  --chunksize=40 --generator=segment --override --encoding=OcT
 
 set TEST_NAME=testEncodingHex
 set OUTDIR=.\generated_files\%TEST_NAME%
 mkdir %OUTDIR% 1>NUL 2>NUL
-testfilegenerator.exe --file=%OUTDIR%\%TEST_NAME%.bin --size=10000 --fill=sequential
-bin2cpp.exe --noheader --file=%OUTDIR%\%TEST_NAME%.bin --output=%OUTDIR% --headerfile=_%TEST_NAME%.h --identifier=%TEST_NAME%  --chunksize=450 --generator=segment --override --encoding=HeX
+testfilegenerator.exe --file=%OUTDIR%\%TEST_NAME%.bin --size=300 --fill=sequential
+bin2cpp.exe --noheader --file=%OUTDIR%\%TEST_NAME%.bin --output=%OUTDIR% --headerfile=_%TEST_NAME%.h --identifier=%TEST_NAME%  --chunksize=40 --generator=segment --override --encoding=HeX
 
 set TEST_NAME=testIssue12
 set OUTDIR=.\generated_files\%TEST_NAME%
