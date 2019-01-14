380ff9801d27b7c805803401ed8eb15d6437f2ae
Antoine Beauchamp <end2endzone@gmail.com>
2019-01-14 05:59:29 -0500

    Modified CMake scripts for Linux to copy the file generation script to CMAKE_CURRENT_BINARY_DIR to allow unit test compilation and to build/bin directory to allow unit test execution.

--- test/bin2cpp_unittest/prebuild.cmake
+++ test/bin2cpp_unittest/prebuild.cmake
@@ -15,8 +15,16 @@ message(STATUS "BIN2CPP_UNITTEST_SOURCE_DIR=  " ${BIN2CPP_UNITTEST_SOURCE_DIR})
 message(STATUS "CMAKE_CURRENT_SOURCE_DIR=     " ${CMAKE_CURRENT_SOURCE_DIR})
 message(STATUS "CMAKE_CURRENT_BINARY_DIR=     " ${CMAKE_CURRENT_BINARY_DIR})
 message(STATUS "CMAKE_CURRENT_LIST_DIR=       " ${CMAKE_CURRENT_LIST_DIR})
+message(STATUS "CMAKE_SOURCE_DIR=             " ${CMAKE_SOURCE_DIR})
+message(STATUS "CMAKE_BINARY_DIR=             " ${CMAKE_BINARY_DIR})
 
-# Generate test file for current configuration.
+# Prebuild script for building unit tests...
+# Note this script is also the one used by the install package.
+message("Configuring prebuild script file '${CMAKE_CURRENT_BINARY_DIR}/generate_test_files.sh' for building unit tests...")
 configure_file(${BIN2CPP_UNITTEST_SOURCE_DIR}/generate_test_files.sh.in ${CMAKE_CURRENT_BINARY_DIR}/generate_test_files.sh @ONLY)
+
+# Script for running unit tests on Travis CI...
+message("Configuring script file '${CMAKE_BINARY_DIR}/generate_test_files.sh' for running unit tests...")
+configure_file(${BIN2CPP_UNITTEST_SOURCE_DIR}/generate_test_files.sh.in ${CMAKE_BINARY_DIR}/generate_test_files.sh @ONLY)
 
 message("${CMAKE_CURRENT_LIST_FILE} - END")
