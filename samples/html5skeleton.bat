80fdd135ed7bbb30dabed827872491c3a2defc11
Antoine <end2endzone@gmail.com>
2019-01-22 00:40:00 -0500

    Refreshed README.md with changes from demo sample.

--- samples/html5skeleton.bat
+++ samples/html5skeleton.bat
@@ -3,8 +3,8 @@
 rmdir /S /Q outdir
 mkdir outdir
 
-set path=%PATH%;%cd%\..\msvc\Win32\Release
+set path=%PATH%;%cd%\..\build\bin\Release
 
-bin2cpp.exe --file=html5skeleton.html --output=.\outdir --headerfile=resourcehtml5skeleton.h --identifier=HtmlSample --chunksize=50
+bin2cpp.exe --file=html5skeleton.html --output=.\outdir --headerfile=file_html5skeleton.h --identifier=HtmlSample --chunksize=50
 
 pause
