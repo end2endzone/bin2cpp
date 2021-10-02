b66d69e54fc91b9631d760a531bf8551365c5ca5
Antoine <end2endzone@gmail.com>
2021-10-02 11:14:16 -0400

    Fixed TestCommon.* unit tests on Linux and macOS.

--- test/bin2cpp_unittest/TestCommon.cpp
+++ test/bin2cpp_unittest/TestCommon.cpp
@@ -29,7 +29,16 @@
 #include "rapidassist/testing.h"
 #include "rapidassist/filesystem.h"
 #include "rapidassist/testing.h"
+#include "rapidassist/user.h"
 
+const std::string slash = ra::filesystem::GetPathSeparatorStr();
+const std::string home_mp3_directory = ra::user::GetHomeDirectory() + slash + "mp3";
+const std::string temp_directory = ra::filesystem::GetTemporaryDirectory();
+#ifdef _WIN32
+const std::string www_directory = "C:\\inetpub\\wwwroot";
+#else
+const std::string www_directory = "/var/www";
+#endif
 
 void TestCommon::SetUp()
 {
@@ -41,10 +50,16 @@ void TestCommon::TearDown()
 
 TEST_F(TestCommon, testGetFunctionIdentifierFromPath)
 {
-  ASSERT_EQ("themedarkcss", bin2cpp::getFunctionIdentifierFromPath("c:\\temp\\www\\static\\css\\theme.dark.css"));
+  std::string path;
+
+  path = www_directory + "\\static\\css\\theme.dark.css";
+  ra::filesystem::NormalizePath(path);
+  ASSERT_EQ("themedarkcss", bin2cpp::getFunctionIdentifierFromPath(path));
 
   //test running twice returns the same value
-  ASSERT_EQ("themedarkcss", bin2cpp::getFunctionIdentifierFromPath("c:\\temp\\www\\static\\css\\theme.dark.css"));
+  path = www_directory + "\\static\\css\\theme.dark.css";
+  ra::filesystem::NormalizePath(path);
+  ASSERT_EQ("themedarkcss", bin2cpp::getFunctionIdentifierFromPath(path));
 
   //test with only a filename
   ASSERT_EQ("themedarkcss", bin2cpp::getFunctionIdentifierFromPath("theme.dark.css"));
@@ -59,33 +74,36 @@ TEST_F(TestCommon, testGetFunctionIdentifierFromPath)
 TEST_F(TestCommon, testGetUniqueFunctionIdentifierFromPath)
 {
   bin2cpp::Dictionary tmp;
+  std::string path;
 
   //test normal scenario
   {
-    const std::string input_path = "c:\\temp\\www\\static\\css\\theme.dark.css";
+    path = www_directory + "\\static\\css\\theme.dark.css";
+    ra::filesystem::NormalizePath(path);
 
-    ASSERT_EQ("themedarkcss", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
+    ASSERT_EQ("themedarkcss", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
 
     //test duplicates
-    ASSERT_EQ("themedarkcss_1", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
-    ASSERT_EQ("themedarkcss_2", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
-    ASSERT_EQ("themedarkcss_3", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
-    ASSERT_EQ("themedarkcss_4", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
-    ASSERT_EQ("themedarkcss_5", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
+    ASSERT_EQ("themedarkcss_1", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
+    ASSERT_EQ("themedarkcss_2", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
+    ASSERT_EQ("themedarkcss_3", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
+    ASSERT_EQ("themedarkcss_4", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
+    ASSERT_EQ("themedarkcss_5", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
   }
 
   //test behavior with files that ends with numbers
   {
-    const std::string input_path = "c:\\test\\index.001";
+    path = temp_directory + "\\index.001";
+    ra::filesystem::NormalizePath(path);
 
-    ASSERT_EQ("index001", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
+    ASSERT_EQ("index001", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
 
     //test duplicates
-    ASSERT_EQ("index001_1", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
-    ASSERT_EQ("index001_2", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
-    ASSERT_EQ("index001_3", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
-    ASSERT_EQ("index001_4", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
-    ASSERT_EQ("index001_5", bin2cpp::getUniqueFunctionIdentifierFromPath(input_path, tmp));
+    ASSERT_EQ("index001_1", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
+    ASSERT_EQ("index001_2", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
+    ASSERT_EQ("index001_3", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
+    ASSERT_EQ("index001_4", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
+    ASSERT_EQ("index001_5", bin2cpp::getUniqueFunctionIdentifierFromPath(path, tmp));
   }
 
   //try to bug the system: get an identifier from a file which is already a duplicate identifer.
@@ -93,26 +111,34 @@ TEST_F(TestCommon, testGetUniqueFunctionIdentifierFromPath)
 
   //test getting duplicate identifiers because of invalid characters
   {
-    ASSERT_EQ("jqueryjs",   bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\foo\\jquery.js", tmp));
+    path = www_directory + "\\static\\js";
+    ra::filesystem::NormalizePath(path);
+    path += slash;
+
+    ASSERT_EQ("jqueryjs",   bin2cpp::getUniqueFunctionIdentifierFromPath(path + "jquery.js", tmp));
 
     //test duplicates
-    ASSERT_EQ("jqueryjs_1", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\foo\\j.query.js"   , tmp));
-    ASSERT_EQ("jqueryjs_2", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\foo\\j!qu!ery.js"  , tmp));
-    ASSERT_EQ("jqueryjs_3", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\foo\\j-query.js"   , tmp));
-    ASSERT_EQ("jqueryjs_4", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\foo\\jqu.er.y.js"  , tmp));
-    ASSERT_EQ("jqueryjs_5", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\foo\\jqueryjs"     , tmp));
+    ASSERT_EQ("jqueryjs_1", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "j.query.js"   , tmp));
+    ASSERT_EQ("jqueryjs_2", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "j!qu!ery.js"  , tmp));
+    ASSERT_EQ("jqueryjs_3", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "j-query.js"   , tmp));
+    ASSERT_EQ("jqueryjs_4", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "jqu.er.y.js"  , tmp));
+    ASSERT_EQ("jqueryjs_5", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "jqueryjs"     , tmp));
   }
 
   //test getting duplicates from different directories
   {
-    ASSERT_EQ("indexhtml",   bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\www\\index.html", tmp));
+    path = www_directory;
+    ra::filesystem::NormalizePath(path);
+    path += slash;
+
+    ASSERT_EQ("indexhtml",   bin2cpp::getUniqueFunctionIdentifierFromPath(path + "index.html", tmp));
 
     //test duplicates
-    ASSERT_EQ("indexhtml_1", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\www\\home\\index.html"                            , tmp));
-    ASSERT_EQ("indexhtml_2", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\www\\blog\\index.html"                            , tmp));
-    ASSERT_EQ("indexhtml_3", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\www\\blog\\how-to-create-a-web-site\\index.html"  , tmp));
-    ASSERT_EQ("indexhtml_4", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\www\\contact\\index.html"                         , tmp));
-    ASSERT_EQ("indexhtml_5", bin2cpp::getUniqueFunctionIdentifierFromPath("c:\\www\\blog\\using-bin2cpp\\index.html"             , tmp));
+    ASSERT_EQ("indexhtml_1", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "home"     + slash + "index.html"                                        , tmp));
+    ASSERT_EQ("indexhtml_2", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "blog"     + slash + "index.html"                                        , tmp));
+    ASSERT_EQ("indexhtml_3", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "blog"     + slash + "how-to-create-a-web-site" + slash + "index.html"   , tmp));
+    ASSERT_EQ("indexhtml_4", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "contact"  + slash + "index.html"                                        , tmp));
+    ASSERT_EQ("indexhtml_5", bin2cpp::getUniqueFunctionIdentifierFromPath(path + "blog"     + slash + "using-bin2cpp" + slash + "index.html"              , tmp));
   }
 }
 
@@ -120,17 +146,22 @@ TEST_F(TestCommon, testGetUniqueFilePath)
 {
   bin2cpp::Dictionary dict;
   std::string path;
+  std::string base_dir;
 
   //test basic directory and filename path
   {
-    path = "C:\\temp\\index.h";
-    ASSERT_EQ("C:\\temp\\index.h", bin2cpp::getUniqueFilePath(path, dict));
+    path = www_directory + slash + "index.h";
+    ra::filesystem::NormalizePath(path);
+    base_dir = ra::filesystem::GetParentPath(path);
+    base_dir += slash;
 
-    ASSERT_EQ("C:\\temp\\index_1.h", bin2cpp::getUniqueFilePath(path, dict));
-    ASSERT_EQ("C:\\temp\\index_2.h", bin2cpp::getUniqueFilePath(path, dict));
-    ASSERT_EQ("C:\\temp\\index_3.h", bin2cpp::getUniqueFilePath(path, dict));
-    ASSERT_EQ("C:\\temp\\index_4.h", bin2cpp::getUniqueFilePath(path, dict));
-    ASSERT_EQ("C:\\temp\\index_5.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index.h", bin2cpp::getUniqueFilePath(path, dict));
+
+    ASSERT_EQ(base_dir + "index_1.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index_2.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index_3.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index_4.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index_5.h", bin2cpp::getUniqueFilePath(path, dict));
   }
 
   //test filename only
@@ -148,13 +179,17 @@ TEST_F(TestCommon, testGetUniqueFilePath)
   //test relative paths
   {
     path = "www\\blog\\index.h";
-    ASSERT_EQ("www\\blog\\index.h", bin2cpp::getUniqueFilePath(path, dict));
+    ra::filesystem::NormalizePath(path);
+    base_dir = ra::filesystem::GetParentPath(path);
+    base_dir += slash;
 
-    ASSERT_EQ("www\\blog\\index_1.h", bin2cpp::getUniqueFilePath(path, dict));
-    ASSERT_EQ("www\\blog\\index_2.h", bin2cpp::getUniqueFilePath(path, dict));
-    ASSERT_EQ("www\\blog\\index_3.h", bin2cpp::getUniqueFilePath(path, dict));
-    ASSERT_EQ("www\\blog\\index_4.h", bin2cpp::getUniqueFilePath(path, dict));
-    ASSERT_EQ("www\\blog\\index_5.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index.h", bin2cpp::getUniqueFilePath(path, dict));
+
+    ASSERT_EQ(base_dir + "index_1.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index_2.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index_3.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index_4.h", bin2cpp::getUniqueFilePath(path, dict));
+    ASSERT_EQ(base_dir + "index_5.h", bin2cpp::getUniqueFilePath(path, dict));
   }
 }
 
@@ -166,32 +201,44 @@ TEST_F(TestCommon, testPathSplitJoin)
   std::string file_ext;
 
   // test split
-  path = "C:\\Windows\\notepad.exe";
+  path = home_mp3_directory + slash + "music.mp3";
   bin2cpp::pathSplit(path, dir, file_name, file_ext);
-  ASSERT_EQ("C:\\Windows"     , dir);
-  ASSERT_EQ("notepad"         , file_name);
-  ASSERT_EQ("exe"             , file_ext);
+  ASSERT_EQ(home_mp3_directory, dir);
+  ASSERT_EQ("music"           , file_name);
+  ASSERT_EQ("mp3"             , file_ext);
 
   // test join
-  dir = "C:\\Windows";
-  file_name = "notepad";
-  file_ext = "exe";
+  dir = home_mp3_directory;
+  file_name = "music";
+  file_ext = "mp3";
   path = bin2cpp::pathJoin(dir, file_name, file_ext);
-  ASSERT_EQ("C:\\Windows\\notepad.exe", path);
+  ASSERT_EQ(home_mp3_directory + slash + "music.mp3", path);
 
   static const char * paths[] = {
-    "C:\\Windows\\notepad.exe",       // absolute file path
     "notepad.exe",                    // filename only
     "dumpfile",                       // filename without an extension
     "dump.file.",                     // file name with a dot in name but without an extension
+#ifdef _WIN32
+    "C:\\Windows\\notepad.exe",       // absolute file path
     "C:\\temp\\dumpfile",             // directory and filename without an extension
     "C:\\temp\\dump.file.",           // directory and file name with a dot in name
     "C:\\Windows",                    // only a directory
-    "C:\\temp\\.tmp",                 // an file without a file name
+    "C:\\temp\\.tmp",                 // a file without a file name
     "C:\\pagefile.sys",               // file in root directory
     "C:\\bootmgr",                    // file in root directory without an extension
     "C:\\",                           // the root directory
     "www\\blog\\index.html",          // test relative path
+#else
+    "/home/mp3/music.mp3",            // absolute file path
+    "/tmp/dumpfile",                  // directory and filename without an extension
+    "/tmp/dump.file.",                // directory and file name with a dot in name
+    "/home/dev",                      // only a directory
+    "/tmp/.tmp",                      // a file without a file name
+    "/bootfile.sys",                  // file in root directory
+    "/bootmgr",                       // file in root directory without an extension
+    "/",                              // the root directory
+    "/var/www/blog/index.html",       // test relative path
+#endif
   };
   static const size_t num_paths = sizeof(paths)/sizeof(paths[0]);
   for(size_t i=0; i<num_paths; i++)
