end2endzone/bin2cpp

commit 7b4b643ab79d0f0f1bfbb814b22db9444b3e54b2

Fixed the following warnings on Windows: D:\bin2cpp\src\bin2cpp\SegmentGenerator.cpp(136): warning C4477: 'fprintf' : format string '%lu' requires an argument of type 'unsigned long', but variadic argument 2 has type 'size_t' D:\bin2cpp\samples\demo_helloworld\helloworld.cpp(11): warning C4477: 'printf' : format string '%lu' requires an argument of type 'unsigned long', but variadic argument 2 has type 'size_t' D:\bin2cpp\samples\demo_icons\demo_icons.cpp(12): warning C4477: 'printf' : format string '%lu' requires an argument of type 'unsigned long', but variadic argument 1 has type 'size_t' D:\bin2cpp\samples\demo_icons\demo_icons.cpp(18): warning C4477: 'printf' : format string '%lu' requires an argument of type 'unsigned long', but variadic argument 2 has type 'size_t'

samples/demo_helloworld/helloworld.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
   const bin2cpp::File & resource = bin2cpp::getHelloWorldHtmlFile();
 
   //print information about the file.
-  printf("Embedded file '%s' is %lu bytes long.\n", resource.getFilename(), resource.getSize());
+  printf("Embedded file '%s' is %llu bytes long.\n", resource.getFilename(), resource.getSize());
 
   //Saving content back to a file.
   printf("Saving embedded file to 'helloworld_copy.html'...\n");

samples/demo_icons/demo_icons.cpp
@@ -9,13 +9,13 @@ int main(int argc, char* argv[])
 
   //Print information about all files generated with "--managerfile" or --registerfile flags.
   size_t num_files = mgr.getFileCount();
-  printf("Found %lu embedded icons...\n", num_files);
+  printf("Found %llu embedded icons...\n", num_files);
 
   //Listing files.
   for(size_t i=0; i<num_files; i++)
   {
     const bin2cpp::File * file = mgr.getFile(i);
-    printf("  File '%s', %lu bytes\n", file->getFilename(), file->getSize());
+    printf("  File '%s', %llu bytes\n", file->getFilename(), file->getSize());
   }
 
   //Saving content back to files.

src/bin2cpp/SegmentGenerator.cpp
@@ -133,7 +133,7 @@ namespace bin2cpp
       };
 
       //output
-      fprintf(cpp, "      mBuffer.append(\"%s\", %lu);\n", cppEncoder.c_str(), readSize);
+      fprintf(cpp, "      mBuffer.append(\"%s\", %llu);\n", cppEncoder.c_str(), readSize);
     }
     delete[] buffer;
     buffer = NULL;

commit 2d26da267802133352087495d6ae35e295cf3d95

Fixed warning bug using printf with %llu on Raspberry Pi introduced in 7b4b643ab79d0f0f1bfbb814b22db9444b3e54b2.

samples/demo_helloworld/helloworld.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string>
+#include <iostream>
+
 #include "generated_helloworld.h" //a single include file is all you need
 
 int main(int argc, char* argv[])
@@ -8,15 +10,15 @@ int main(int argc, char* argv[])
   const bin2cpp::File & resource = bin2cpp::getHelloWorldHtmlFile();
 
   //print information about the file.
-  printf("Embedded file '%s' is %llu bytes long.\n", resource.getFilename(), resource.getSize());
+  std::cout << "Embedded file '" << resource.getFilename() << "' is " << resource.getSize() << " bytes long.\n";
 
   //Saving content back to a file.
-  printf("Saving embedded file to 'helloworld_copy.html'...\n");
+  std::cout << " Saving embedded file to 'helloworld_copy.html'...\n";
   bool saved = resource.save("helloworld_copy.html");
   if (saved)
-    printf("saved\n");
+    std::cout << "saved\n";
   else
-    printf("failed\n");
+    std::cout << "failed\n";
 
   //Get the internal buffer and do something with the binary data
   const char * buffer = resource.getBuffer();

samples/demo_icons/demo_icons.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>  // printf
 #include <stdlib.h> // getenv
+#include <iostream>
 
 #include "IconsFileManager.h"
 
@@ -9,23 +10,23 @@ int main(int argc, char* argv[])
 
   //Print information about all files generated with "--managerfile" or --registerfile flags.
   size_t num_files = mgr.getFileCount();
-  printf("Found %llu embedded icons...\n", num_files);
+  std::cout << "Found " << num_files << " embedded icons...\n";
 
   //Listing files.
   for(size_t i=0; i<num_files; i++)
   {
     const bin2cpp::File * file = mgr.getFile(i);
-    printf("  File '%s', %llu bytes\n", file->getFilename(), file->getSize());
+    std::cout << "  File '" << file->getFilename() << "', " << file->getSize() << " bytes\n";
   }
 
   //Saving content back to files.
   const char * temp_dir = getenv("TEMP");
-  printf("Saving embedded icons to directory '%s'...\n", temp_dir);
+  std::cout << "Saving embedded icons to directory '" << temp_dir << "'...\n";
   bool saved = mgr.saveFiles(temp_dir);
   if (saved)
-    printf("saved\n");
+    std::cout << "saved\n";
   else
-    printf("failed\n");
+    std::cout << "failed\n";
 
   return 0;
 }

src/bin2cpp/SegmentGenerator.cpp
@@ -134,7 +134,7 @@ namespace bin2cpp
       };
 
       //output
-      fprintf(cpp, "      mBuffer.append(\"%s\", %llu);\n", cppEncoder.c_str(), readSize);
+      fprintf(cpp, "      mBuffer.append(\"%s\", %s);\n", cppEncoder.c_str(), ra::strings::ToString(readSize).c_str());
     }
     delete[] buffer;
     buffer = NULL;